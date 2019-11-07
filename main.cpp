566a6bef866bb71921ed9907c3167d515de8b707
Greg James <madra.uc@gmail.com>
2019-11-07 13:17:37 -0600

    Added files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "OctaneEngine.hpp"
+
+int main()
+{
+    OctaneEngine file = OctaneEngine("https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4", "snake.mp4");
+
+    file.get_url();
+    file.get_filesize();
+    file.get_partsize();
+
+    file.download_file();
+    return 0;
+}
